d6cc71fb7755a26a286841773d33a5c3d667ff4e
Dzeneta Ahmic <dahmic3@etf.unsa.ba>
2021-11-19 23:38:59 +0100

    Dodan testni projekat

---
+++ TestCvjecara/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Cvjecara;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCvjecara
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
