cf1e6c5025a92307ab8d13ff7b2010f779ac9a80
Dzeneta Ahmic <dahmic3@etf.unsa.ba>
2021-11-20 22:58:11 +0100

    Dodani neki testovi za metodu DajSvjezinuCvijeca u klasi Cvijet

---
+++ TestCvjecara/UnitTestsCvijet.cs
@@ -0,0 +1,44 @@
+﻿using Cvjecara;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+namespace TestCvjecara
+{
+    [TestClass]
+    public class UnitTestsCvijet
+    {
+        Cvijet c1, c2, c3;
+
+        [TestInitialize]
+        public void InicijalizacijaPrijeSvakogTesta()
+        {
+            c1 = new Cvijet(Vrsta.Ruža, "Majska", "Crvena", DateTime.Now.AddDays(-2), 1);
+            c2 = new Cvijet(Vrsta.Orhideja, "Orhideja", "Bijela", DateTime.Now.AddDays(-3), 2);
+            c3 = new Cvijet(Vrsta.Ljiljan, "Bosanski ljiljan", "Žuta", DateTime.Now.AddDays(-5), 1);
+        }
+        [TestMethod]
+        public void TestNajboljaSvježinaCvijeća()
+        {
+            Assert.AreEqual(c1.OdrediSvježinuCvijeća(), 5.0);
+
+        }
+        [TestMethod]
+        public void TestSmanjeneSvjezineOrhideja()
+        {
+            Assert.AreEqual(c2.OdrediSvježinuCvijeća(), 4.5);
+        }
+
+        [TestMethod]
+        public void TestSmanjeneSvjezineLjiljan()
+        {
+            Assert.AreEqual(c3.OdrediSvježinuCvijeća(), 2.6);
+        }
+        [TestMethod]
+        public void TestSvjezinaNula()
+        {
+            c3.DatumBranja = DateTime.Now.AddDays(-7);
+            Assert.AreEqual(c3.OdrediSvježinuCvijeća(), 0.0);
+        }
+
+    }
+}
